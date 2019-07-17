38358a4a84a4b4a78e5695e30c24ad4add7d8646
Stephen Senft <stephensenft21@gmail.com>
2019-07-17 04:29:41 -0500

    final commit i have questions about this one @jisie

---
+++ overly-excited.js
@@ -0,0 +1,21 @@
+
+
+// Create an array that contains the words in the sentence
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the moon"];
+
+// The addExcitement function should accept the array as the sole argument
+ function addExcitement (theWordArray,character,multiplier) {
+    let buildMeUp = " "
+    for (let i = 0; i < theWordArray.length; i++) {
+
+        if ((i + 1) % 3 === 0 && i !== 0) {
+
+            buildMeUp += theWordArray[i] + character.repeat(multiplier) + " ";
+
+        } else {
+            buildMeUp += theWordArray[i] + " ";
+        }
+       console.log(sentence)
+        
+    }}
+    addExcitement(sentence, "!")
